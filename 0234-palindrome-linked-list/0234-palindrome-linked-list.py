429014cc1e24f5860df475b14c8468d380017c8d
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2025-10-06 11:48:46 -0400

    Time: 137 ms (40.4%), Space: 66.2 MB (87.38%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -9,21 +9,28 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: bool
         """
-        slow=fast=head
+        if not head.next:
+            return True
+
+        slow=head
+        fast=head.next
         while fast and fast.next:
             slow=slow.next
             fast=fast.next.next
         
-        node=None
-        while slow:
-            face=slow.next
-            slow.next=node
-            node=slow
-            slow=face
-        left,right=head,node
+        prev=None
+        curr=slow.next
+        while curr:
+            face=curr.next
+            curr.next=prev
+            prev=curr
+            curr=face
+        left=head
+        right=prev
         while right:
             if left.val!=right.val:
                 return False
             left=left.next
             right=right.next
+        
         return True
