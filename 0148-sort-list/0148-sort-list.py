57aa7065fb890925bde0bf02cbabaae740f25de5
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2025-10-07 17:47:02 -0400

    Time: 419 ms (64%), Space: 51 MB (40%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -9,39 +9,32 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        #base-case
         if not head or not head.next:
             return head
         
-        #find the mid
-        slow,fast=head,head.next
+        slow=head
+        fast=head.next
+
         while fast and fast.next:
             slow=slow.next
             fast=fast.next.next
-        
-        #handle the last nodes correctly
         mid=slow.next
         slow.next=None
-
-        #split both halves
         left=self.sortList(head)
         right=self.sortList(mid)
 
         return self.merge(left,right)
     
     def merge(self,left,right):
-        dummy = ListNode()
-        ansLL=dummy
-
+        dummyN=ListNode(-1)
+        temp=dummyN
         while left and right:
             if left.val<=right.val:
-                ansLL.next=left
+                temp.next=left
                 left=left.next
             else:
-                ansLL.next=right
+                temp.next=right
                 right=right.next
-            ansLL=ansLL.next
-        
-        ansLL.next=left if left else right
-        
-        return dummy.next
+            temp=temp.next
+        temp.next=left if left else right
+        return dummyN.next
