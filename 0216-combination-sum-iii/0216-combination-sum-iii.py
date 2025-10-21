0b97d8ae3297691c750ec9aa21893699f6192097
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2025-10-21 13:08:05 -0400

    Time: 2 ms (49.68%), Space: 12.5 MB (12.18%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -5,23 +5,25 @@ class Solution(object):
         :type n: int
         :rtype: List[List[int]]
         """
-        arr=[1,2,3,4,5,6,7,8,9]
-        ans=[]
+        num=[1,2,3,4,5,6,7,8,9]
         res=[]
-
+        ans=[]
         def getCombination(i,temp):
-            if temp==n and len(ans)==k:
+            if temp==0 and len(ans)==k:
                 res.append(ans[:])
                 return
             
-            if i>=len(arr) or temp>n:
+            if i==len(num) or num[i]>temp:
                 return
             
-            ans.append(arr[i])
-            getCombination(i+1,temp+arr[i])
+            
+            
+            #pick
+            ans.append(num[i])
+            getCombination(i+1,temp-num[i])
+            #not-pick
             ans.pop()
-
             getCombination(i+1,temp)
         
-        getCombination(0,0)
+        getCombination(0,n)
         return res
