2cbe5ff87b36d0e1959435d01c535ec554490954
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2025-10-29 18:34:35 -0400

    Time: 2078 ms (48.62%), Space: 12.5 MB (79.93%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -1,36 +1,43 @@
-class Solution(object):
+class Solution:
     def solveSudoku(self, board):
-        """
-        :type board: List[List[str]]
-        :rtype: None Do not return anything, modify board in-place instead.
-        """
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
+        empty = []
 
+        # Initialize sets and empty cells
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    empty.append((r, c))
+                else:
+                    val = board[r][c]
+                    rows[r].add(val)
+                    cols[c].add(val)
+                    boxes[(r//3)*3 + (c//3)].add(val)
 
-        ch=['1','2','3','4','5','6','7','8','9']
-        def isValid(r,c,char):
-            for i in range(9):
-                if board[r][i]==char or board[i][c]==char:
-                    return False
-            startRow=(r//3)*3
-            startCol=(c//3)*3
-            for i in range(3):
-                for j in range(3):
-                    if board[startRow+i][startCol+j]==char:
-                        return False
-            
-            return True
+        def solve(idx=0):
+            if idx == len(empty):
+                return True  # all cells filled
 
-        def solve():
-            for r in range(9):
-                for c in range(9):
-                    if board[r][c]=='.':
-                        for char in ch:
-                            if isValid(r,c,char):
-                                board[r][c]=char
-                                if solve():
-                                    return True
-                                board[r][c]='.'
-                        return False
-            return True
+            r, c = empty[idx]
+            box_id = (r//3)*3 + (c//3)
+
+            for num in '123456789':
+                if num not in rows[r] and num not in cols[c] and num not in boxes[box_id]:
+                    board[r][c] = num
+                    rows[r].add(num)
+                    cols[c].add(num)
+                    boxes[box_id].add(num)
+
+                    if solve(idx+1):
+                        return True
+
+                    # backtrack
+                    board[r][c] = '.'
+                    rows[r].remove(num)
+                    cols[c].remove(num)
+                    boxes[box_id].remove(num)
+            return False
 
         solve()
