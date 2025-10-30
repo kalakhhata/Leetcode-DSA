9ff603a7e96046d00951f0a70bf3dd45b0992535
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2025-10-30 17:54:43 -0400

    Time: 4 ms (6.41%), Space: 12.2 MB (98.72%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -4,18 +4,24 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        MOD=(10**9)+7
-        even=(n+1)//2
-        odd=n//2
-        return (self.powC(5,even)*self.powC(4,odd))%MOD
-    
-    def powC(self,x,n):
-        MOD=(10**9)+7
         if n==0:
             return 1
-        half=self.powC(x,n//2)
+        
+        MOD=10**(9)+7
+
+        even=(n+1)//2
+        odd=n//2
+
+        return (self.getAns(5,even)*self.getAns(4,odd))%MOD
+    
+    def getAns(self,x,n):
+        MOD=10**(9)+7
+        if n==0:
+            return 1
+        
+        half=self.getAns(x,n//2)
         if n%2==0:
             return (half*half)%MOD
         else:
             return (half*half*x)%MOD
-        
+            
