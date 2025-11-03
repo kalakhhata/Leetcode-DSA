cf87f96793d284d3ccb91d56fe7e2714db2a534d
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2025-11-03 14:55:29 -0500

    Time: 4 ms (11.49%), Space: 12.5 MB (52.49%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.py
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -5,29 +5,30 @@ class Solution(object):
         :type divisor: int
         :rtype: int
         """
-        INT_MAX=2**31 -1
+        INT_MAX=2**31-1
         INT_MIN=-2**31
 
         if dividend==INT_MIN and divisor==-1:
             return INT_MAX
         
-        negative = (dividend<0) != (divisor<0)
+        negative = (dividend<0)!=(divisor<0)
 
         dividend=abs(dividend)
         divisor=abs(divisor)
+        
         quotint=0
-
         while dividend>=divisor:
             temp=divisor
             multiply=1
             while dividend>=(temp<<1):
                 temp=temp<<1
                 multiply=multiply<<1
-            quotint+=multiply
             dividend-=temp
+            quotint+=multiply
         
         if negative:
             quotint=-quotint
         
         return max(min(quotint,INT_MAX),INT_MIN)
-            
+
+        
