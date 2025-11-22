68ec05a11ff5ceefdd9e6160b49fa9c7dfb741a1
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2025-11-22 16:23:23 -0500

    Time: 35 ms (67.13%), Space: 14.4 MB (29.68%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.py
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -4,15 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        n=len(nums)
         stack=[]
-        n=len(nums)
-        result=[-1]*n
-        n=len(nums)
+        nge=[-1]*n
 
         for i in range(2*n):
             while stack and nums[stack[-1]]<nums[i%n]:
-                result[stack.pop()]=nums[i%n]
+                nge[stack.pop()]=nums[i%n]
             if i<n:
                 stack.append(i)
         
-        return result
+        return nge
