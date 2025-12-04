5823f5305d8427fdee6f1d39388022b571bc32a3
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2025-12-03 20:40:58 -0500

    Time: 142 ms (41.89%), Space: 16.3 MB (69.08%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -5,20 +5,21 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        
+
         def atMost(i):
             if i<0:
                 return 0
-            count=0
             l=0
             currsum=0
+            count=0
             for r in range(len(nums)):
                 currsum+=nums[r]%2
-                
+
                 while currsum>i:
                     currsum-=nums[l]%2
                     l+=1
-                count+=(r-l)+1
+                count+=(r-l+1)
             
             return count
-        
         return atMost(k)-atMost(k-1)
