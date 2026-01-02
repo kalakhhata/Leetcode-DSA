959851178ed988869b46fc3d2860da1878dcfeb1
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-01-02 10:46:58 -0500

    Time: 30 ms (79.51%), Space: 14.1 MB (76.81%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def findContentChildren(self, g, s):
+        """
+        :type g: List[int]
+        :type s: List[int]
+        :rtype: int
+        """
+        g.sort()
+        s=sorted(s)
+        i=0
+        j=0
+        cnt=0
+
+        while i<len(g) and j<len(s):
+            if g[i]<=s[j]:
+                cnt+=1
+                i+=1
+                j+=1
+            else:
+                j+=1
+        
+        return cnt
