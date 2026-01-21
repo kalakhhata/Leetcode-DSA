f646f70513034c8a5b9694ad830bf25952159938
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-01-21 12:36:28 -0500

    Time: 0 ms (100%), Space: 23.8 MB (21.37%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        curr=root
+        lh=0
+        while curr:
+            curr=curr.left
+            lh+=1
+        curr=root
+        rh=0
+        while curr:
+            curr=curr.right
+            rh+=1
+        
+        if lh==rh:
+            return (1<<lh)-1
+        
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+
+
+        
