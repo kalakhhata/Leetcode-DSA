3bd2683e3c520eb8448ff4e655ca1c09433def38
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-02-10 12:50:59 -0500

    Time: 3 ms (69.46%), Space: 19.5 MB (13.11%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.py
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.py
@@ -1,30 +1,29 @@
-class Solution(object):
-    def splitArray(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: int
-        """
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
         low=max(nums)
         high=sum(nums)
+
         while low<=high:
             mid=low+(high-low)//2
-            if self.canSplit(nums,mid,k):
+            ans=self.canSplit(nums,k,mid)
+
+            if ans:
                 high=mid-1
             else:
                 low=mid+1
         
         return low
     
-    def canSplit(self,nums,mid,k):
+    def canSplit(self,nums,k,mid):
         cnt=1
-        temp_len=nums[0]
+        s=nums[0]
 
         for i in range(1,len(nums)):
-            if temp_len+nums[i]>mid:
-                cnt+=1
-                temp_len=nums[i]
+            if s+nums[i]<=mid:
+                s+=nums[i]
             else:
-                temp_len+=nums[i]
+                cnt+=1
+                s=nums[i]
+        
         return cnt<=k
         
