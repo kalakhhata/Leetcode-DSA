d15b59b0f69d12ba25b9d42bb55e1c4abce8abda
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-02-19 11:46:15 -0500

    Time: 0 ms (100%), Space: 19.5 MB (21.11%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,10 +1,5 @@
-class Solution(object):
-    def isIsomorphic(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
         char_map={}
         for sc,tc in zip(s,t):
             if sc in char_map:
@@ -13,7 +8,6 @@ class Solution(object):
             elif tc in char_map.values():
                 return False
             char_map[sc]=tc
-        return True
         
-
+        return True
         
