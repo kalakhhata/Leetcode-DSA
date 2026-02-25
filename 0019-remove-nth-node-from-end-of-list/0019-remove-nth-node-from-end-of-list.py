35a52a050020846009a1aaef896c5e5a6d1bfe49
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-02-25 10:48:36 -0500

    Time: 0 ms (100%), Space: 19.3 MB (55.07%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -1,35 +1,31 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def removeNthFromEnd(self, head, n):
-        """
-        :type head: Optional[ListNode]
-        :type n: int
-        :rtype: Optional[ListNode]
-        """
-        if not head.next:
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        if not head or not head.next:
             return None
+        l=0
         curr=head
-        cnt=0
         while curr:
-            cnt+=1
-            curr=curr.next
-            
-        pivot=cnt-n
-        if pivot==0:
-            return head.next
-        curr=head
-        cnt=0
-        while curr:
-            cnt+=1
-            if cnt==pivot:
-                curr.next=curr.next.next
-                break
+            l+=1
             curr=curr.next
         
-
+        pivot=(l-n)-1
+        if pivot==-1:
+            return head.next
+        temp=0
+        curr=head
+        while curr:
             
+            if temp==pivot:
+                curr.next=curr.next.next if curr.next else None
+                break
+            temp+=1
+            curr=curr.next
         return head
+
+
+        
