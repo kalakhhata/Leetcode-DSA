308456656a4965009851174c41b8ea10a2840f36
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-03-05 13:33:26 -0500

    Time: 39 ms (0.72%), Space: 19.4 MB (78.61%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,29 +1,27 @@
-from collections import deque
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        row=len(image)
-        col=len(image[0])
         q=deque()
         q.append((sr,sc))
-        dir=[(0,1),(0,-1),(1,0),(-1,0)]
         token=image[sr][sc]
+        row=len(image)
+        col=len(image[0])
+        dire=[(0,1),(0,-1),(1,0),(-1,0)]
 
         if token==color:
             return image
-
+        
         while q:
-            i,j=q.popleft()
-            image[i][j]=color
-            for r,c in dir:
-                rr=i+r
-                cc=j+c
+            r,c=q.popleft()
+            image[r][c]=color
+
+            for i,j in dire:
+                rr=r+i
+                cc=c+j
                 if 0<=rr<row and 0<=cc<col and image[rr][cc]==token:
                     q.append((rr,cc))
         
         return image
-                        
 
 
-                
 
         
