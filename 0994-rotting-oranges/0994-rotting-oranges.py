b4e80f8c28f9702cfc17f32832a2a931ce57957b
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-03-05 12:58:46 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,34 +1,33 @@
-from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         row=len(grid)
         col=len(grid[0])
+        rotton=deque()
         fresh=0
-        rotten=deque()
+        direction=[(0,1),(0,-1),(-1,0),(1,0)]
+        min=0
 
-        #mark the indicies of all rotten oranges with the fresh oranges count
-        for r in range(row):
-            for c in range(col):
-                if grid[r][c]==2:
-                    rotten.append((r,c))
-                elif grid[r][c]==1:
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j]==2:
+                    rotton.append((i,j))
+                elif grid[i][j]==1:
                     fresh+=1
         
-        minutes=0
-        directions=[(0,1),(0,-1),(-1,0),(1,0)]
-        #bfs to all rotten oranges and see in how much time 
-        #it takes rotten the fresh  4-directionally adjacent oranges
-        while rotten and fresh > 0:
-            for _ in range(len(rotten)):
-                i, j = rotten.popleft()
-                for dr, dc in directions:
-                    rr, cc = i + dr, j + dc
-                    if 0 <= rr < row and 0 <= cc < col and grid[rr][cc] == 1:
-                        grid[rr][cc] = 2
-                        rotten.append((rr, cc))
-                        fresh -= 1
-            minutes += 1
+        while rotton and fresh>0:
+            for _ in range(len(rotton)):
+                r,c=rotton.popleft()
+                for rd,cd in direction:
+                    rr=rd+r
+                    cc=cd+c
+                    if 0<=rr<row and 0<=cc<col and grid[rr][cc]==1:
+                        grid[rr][cc]=2
+                        rotton.append((rr,cc))
+                        fresh-=1
+            min+=1
+        return min if fresh<=0 else -1
+                        
         
-        return minutes if fresh<=0 else -1
+
 
         
