ad31ab5c3ba7e5a8e8a6deea3f0dead5af59a4dc
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-03-13 15:32:40 -0400

    Time: 8 ms (33.03%), Space: 22.4 MB (55.63%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -6,43 +6,35 @@ class Solution:
         r=len(board)
         c=len(board[0])
 
-        #run the dfs to see if there exists a diagonal place where there is still a 0
         def dfs(i,j):
-            if i<0 or i>=r or j<0 or j>=c or board[i][j]!='O':
+            if i<0 or j<0 or i>=r or j>=c or board[i][j]!='O':
                 return
             
-            board[i][j]='T'
+            board[i][j]='-1'
             dfs(i+1,j)
-            dfs(i-1,j)
             dfs(i,j+1)
+            dfs(i-1,j)
             dfs(i,j-1)
-        
-        #check the boarders if there exists a 'O'
+            
+
+
         for i in range(r):
-            #left border
             if board[i][0]=='O':
                 dfs(i,0)
-
-            #right border
             if board[i][c-1]=='O':
                 dfs(i,c-1)
         
         for j in range(c):
-            #top border
             if board[0][j]=='O':
                 dfs(0,j)
-            
-            #bottom border
             if board[r-1][j]=='O':
                 dfs(r-1,j)
-        
         for i in range(r):
             for j in range(c):
                 if board[i][j]=='O':
                     board[i][j]='X'
-                elif board[i][j]=='T':
+                elif board[i][j]=='-1':
                     board[i][j]='O'
+
         
-
-
         
