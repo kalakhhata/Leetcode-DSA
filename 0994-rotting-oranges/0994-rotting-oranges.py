fa14062f4b4ce8ddedfb30920408b808c9e4a82e
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-03-13 11:14:17 -0400

    Time: 3 ms (76.13%), Space: 19.3 MB (72.49%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,33 +1,35 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        row=len(grid)
-        col=len(grid[0])
-        rotton=deque()
+        r=len(grid)
+        c=len(grid[0])
         fresh=0
-        direction=[(0,1),(0,-1),(-1,0),(1,0)]
-        min=0
+        q=deque()
+        ans=0
+        dirs=[(0,1),(0,-1),(1,0),(-1,0)]
 
-        for i in range(row):
-            for j in range(col):
+        for i in range(r):
+            for j in range(c):
                 if grid[i][j]==2:
-                    rotton.append((i,j))
+                    q.append((i,j))
                 elif grid[i][j]==1:
                     fresh+=1
         
-        while rotton and fresh>0:
-            for _ in range(len(rotton)):
-                r,c=rotton.popleft()
-                for rd,cd in direction:
-                    rr=rd+r
-                    cc=cd+c
-                    if 0<=rr<row and 0<=cc<col and grid[rr][cc]==1:
-                        grid[rr][cc]=2
-                        rotton.append((rr,cc))
+        while q and fresh>0:
+
+            for _ in range(len(q)):
+                rr,cc=q.popleft()
+                for rd,cd in dirs:
+                    i=rr+rd
+                    j=cc+cd
+                    if 0<=i<r and 0<=j<c and grid[i][j]==1:
+                        grid[i][j]=2
+                        q.append((i,j))
                         fresh-=1
-            min+=1
-        return min if fresh<=0 else -1
-                        
+            ans+=1
         
+        return ans if fresh<=0 else -1
+
+
 
 
         
