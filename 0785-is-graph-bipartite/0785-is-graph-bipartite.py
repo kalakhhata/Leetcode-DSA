da2ee08ab8cfde931129a02c45e184e724bea5d5
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-03-14 10:13:26 -0400

    Time: 0 ms (100%), Space: 19.8 MB (73.6%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,30 +1,32 @@
-from typing import List
-from collections import deque
-
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        n = len(graph)
-        color = [-1] * n  # -1 means uncolored
+        color=[-1]*len(graph)
 
-        for node in range(n):
-            if color[node] == -1:
-                if not self.check(node, color, graph):
+    
+
+
+        for node in range(len(graph)):
+            if color[node]==-1:
+                if not self.check(node,graph,color):
+                    return False
+        return True
+    
+    def check(self,node,graph,color):
+        q=deque([node])
+        color[node]=0
+
+        while q:
+            curr = q.popleft()
+
+            for ne in graph[curr]:
+                if color[ne] == -1:
+                    color[ne] = 1 - color[curr]
+                    q.append(ne)
+
+                elif color[ne] == color[curr]:
                     return False
         
         return True
+        
 
-    def check(self, start, color, graph):
-        q = deque([start])
-        color[start] = 0
-
-        while q:
-            node = q.popleft()
-
-            for ne in graph[node]:
-                if color[ne] == -1:
-                    color[ne] = 1 - color[node]
-                    q.append(ne)
-                elif color[ne] == color[node]:
-                    return False
-
-        return True
+        
