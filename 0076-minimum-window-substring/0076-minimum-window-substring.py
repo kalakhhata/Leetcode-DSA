5f4ba421d2fd57fc6ac760b77f504f1b3edd1e24
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-04-21 13:35:31 -0400

    Time: 119 ms (54.57%), Space: 15.6 MB (43.61%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -5,29 +5,33 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
-        need=Counter(t)
-        required=len(need)
+        dict_t=Counter(t)
+        need=len(dict_t)
         have=0
-        l=0
-        res=[-1,-1]
         max_len=float('inf')
-        window={}
+        res=[-1,-1]
+        l=0
+        window=defaultdict(int)
+
         for r in range(len(s)):
             c=s[r]
             window[c]=window.get(c,0)+1
-            if c in need and window[c]==need[c]:
+
+            if c in dict_t and window[c]==dict_t[c]:
                 have+=1
             
-            while have==required:
-                
+            while have==need:
                 if r-l+1<max_len:
                     max_len=r-l+1
-                    res=[l,r]
-                window[s[l]]-=1
-                if s[l] in need and window[s[l]]<need[s[l]]:
-                    have-=1
+                    res[0]=l
+                    res[1]=r
                 
+                window[s[l]]-=1
+                if s[l] in dict_t and window[s[l]]<dict_t[s[l]]:
+                    have-=1
                 l+=1
         
         l,r=res
-        return s[l:r+1] if max_len != float("inf") else ""
+
+        return s[l:r+1] if max_len!=float('inf') else ''
+                
