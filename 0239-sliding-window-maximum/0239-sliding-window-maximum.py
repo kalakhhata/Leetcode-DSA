c9006d23580c620c79ca176038e21eed0c6a7535
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-04-21 14:42:38 -0400

    Time: 297 ms (25.33%), Space: 28 MB (80.71%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -7,19 +7,19 @@ class Solution(object):
         """
         ans=[]
         q=deque()
+        l,r=0,0
 
-        for idx,num in enumerate(nums):
-            #firstly we run while loop to check if deque has elements and last element is smaller than the current el we pop out the element to keep the max value at top
-            while q and q[-1]<num:
+        while r<len(nums):
+            while q and nums[q[-1]]<nums[r]:
                 q.pop()
-            q.append(num)
-
-            #not here we check if we are in next window we donts have the previos's window's max elment if so we pop out from front
-
-            if idx>=k and nums[idx-k]==q[0]:
+            q.append(r)
+            
+            if q[0] < l:
                 q.popleft()
-            #here we check we we have crossed k-1 element to make sure if window is generated we add the mac element in result which is at top
-            if idx>=k-1:
-                ans.append(q[0])
+
+            if r+1>=k:
+                ans.append(nums[q[0]])
+                l+=1
+            r+=1
         
         return ans
