3fd59a4e7a959b294567646f026fdb079b2f35c2
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-05-07 09:51:49 -0400

    Time: 3 ms (19.21%), Space: 20.1 MB (25.4%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -1,32 +1,29 @@
+from collections import deque 
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def levelOrder(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: List[List[int]]
-        """
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
-        queue=[]
-        res=[]
-        queue.append(root)
+        ans=[]
+        dq=deque([root])
 
-        while queue:
-            level=[]
-            for i in range(len(queue)):
-                node=queue.pop(0)
-                level.append(node.val)
+        while dq:
+            temp=[]
+            for _ in range(len(dq)):
+                node=dq.popleft()
+                temp.append(node.val)
                 if node.left:
-                    queue.append(node.left)
+                    dq.append(node.left)
                 if node.right:
-                    queue.append(node.right)
-            res.append(level)
-        
-        return res
-            
+                    dq.append(node.right)
+            ans.append(temp)
+        return ans
+
+
+
         
