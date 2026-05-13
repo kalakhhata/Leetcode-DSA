2d656f755d26d91260baaf42c6d91a074b03efde
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-05-13 17:59:13 -0400

    Time: 7 ms (90.28%), Space: 28.8 MB (28.36%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.py
+++ 0355-design-twitter/0355-design-twitter.py
@@ -1,36 +1,38 @@
+import heapq
 class Twitter:
 
     def __init__(self):
-        self.time=0
         self.tweet=defaultdict(list)
         self.following=defaultdict(set)
+        self.time=0
         
 
     def postTweet(self, userId: int, tweetId: int) -> None:
         self.time+=1
         self.tweet[userId].append([self.time,tweetId])
+
         
 
     def getNewsFeed(self, userId: int) -> List[int]:
-
-        user=self.following[userId] | {userId}
         max_heap=[]
         res=[]
+        user= self.following[userId] | {userId}
         for u in user:
             if self.tweet[u]:
-                time,tid=self.tweet[u][-1]
                 idx=len(self.tweet[u])-1
-                heapq.heappush(max_heap,(-time,tid,idx,u))
+                time,tid=self.tweet[u][-1]
+                heapq.heappush(max_heap,(-time,tid,u,idx))
         
         while max_heap and len(res)<10:
-            _,tid,idx,u=heapq.heappop(max_heap)
+            _,tid,u,idx=heapq.heappop(max_heap)
             res.append(tid)
             if idx>0:
-                idx = idx - 1
-                time, tid = self.tweet[u][idx]
-                heapq.heappush(max_heap, (-time, tid, idx, u))
-            
+                idx=idx-1
+                time,tid=self.tweet[u][idx]
+                heapq.heappush(max_heap,(-time,tid,u,idx))
+        
         return res
+
 
         
 
