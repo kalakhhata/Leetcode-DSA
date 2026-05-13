2c78e9bca0ea39014f766dac8007d5c070c156fa
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-05-13 16:30:02 -0400

    Time: 193 ms (47.01%), Space: 43.2 MB (10.21%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -2,34 +2,36 @@ import heapq
 class MedianFinder:
 
     def __init__(self):
-        self.left_heap=[]
-        self.right_heap=[]
+        self.left=[] #max-heap
+        self.right=[] #min-heap
         
 
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.left_heap,-num)
+        heapq.heappush(self.left,-num)
 
-        if self.left_heap and self.right_heap and -self.left_heap[0]>self.right_heap[0]:
-            heapq.heappush(self.right_heap,-heapq.heappop(self.left_heap))
+        if self.right and -self.left[0]>self.right[0]:
+            heapq.heappush(self.right,-heapq.heappop(self.left))
+
+
+        if len(self.right)+1<len(self.left):
+            heapq.heappush(self.right,-heapq.heappop(self.left))
         
-        if len(self.right_heap)>len(self.left_heap)+1:
-            heapq.heappush(self.left_heap,-heapq.heappop(self.right_heap))
+        if len(self.left)+1<len(self.right)+1:
+            heapq.heappush(self.left,-heapq.heappop(self.right))
         
-        if len(self.left_heap)>len(self.right_heap)+1:
-            heapq.heappush(self.right_heap,-heapq.heappop(self.left_heap))
 
 
 
         
 
     def findMedian(self) -> float:
-        if len(self.right_heap)>len(self.left_heap):
-            return float(self.right_heap[0])
+        if len(self.left)==len(self.right):
+            return (self.right[0]-self.left[0])/2
+        elif len(self.left)>len(self.right):
+            return -float(self.left[0])
+        else:
+            return float(self.right[0])
         
-        if len(self.left_heap)>len(self.right_heap):
-            return float(-self.left_heap[0])
-        
-        return (self.right_heap[0]-self.left_heap[0])/2.0
         
 
 
