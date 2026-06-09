09b952c2d728f1700fea8993f58d9b550d02270d
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-06-09 12:41:06 -0400

    Time: 96 ms (55.24%), Space: 28.6 MB (57.92%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,12 +1,10 @@
-class Solution(object):
-    def dailyTemperatures(self, temperatures):
-        """
-        :type temperatures: List[int]
-        :rtype: List[int]
-        """
-        st=[]
-        ans=[0]*len(temperatures)
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
 
+
+
+        ans=[0]*len(temperatures)
+        st=[]
         for i,val in enumerate(temperatures):
             while st and temperatures[st[-1]]<val:
                 idx=st.pop()
@@ -14,5 +12,5 @@ class Solution(object):
             st.append(i)
         
         return ans
-
+            
         
