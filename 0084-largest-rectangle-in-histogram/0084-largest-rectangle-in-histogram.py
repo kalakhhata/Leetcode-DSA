853eb36c78569ef621b9ad7b876808bc0bfc4fcc
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-06-10 07:37:07 -0400

    Time: 208 ms (62.29%), Space: 22.3 MB (52.08%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -4,25 +4,19 @@ class Solution(object):
         :type heights: List[int]
         :rtype: int
         """
-        maxEl=0
+        maxArea=0
         st=[]
-        n=len(heights)
-
-        for i in range(n):
+        for i in range(len(heights)):
             while st and heights[st[-1]]>heights[i]:
                 el=heights[st.pop()]
                 nle=i
                 ple=st[-1] if st else -1
-                maxEl=max(maxEl,el*(nle-ple-1))
+                maxArea=max(maxArea,el*(nle-ple-1))
             st.append(i)
         
         while st:
             el=heights[st.pop()]
-            nle=n
+            nle=len(heights)
             ple=st[-1] if st else -1
-            maxEl=max(maxEl,el*(nle-ple-1))
-        
-        return maxEl
-
-            
-                
+            maxArea=max(maxArea,el*(nle-ple-1))
+        return maxArea
