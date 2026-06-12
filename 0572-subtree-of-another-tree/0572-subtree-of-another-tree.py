79cd3464dc4dca7e3a123bcef7c625263e1827dc
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-06-12 11:52:57 -0400

    Time: 75 ms (79.56%), Space: 13.6 MB (41.94%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -1,19 +1,25 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        def checkNode(node1):
-            if not node1:
+class Solution(object):
+    def isSubtree(self, root, subRoot):
+        """
+        :type root: Optional[TreeNode]
+        :type subRoot: Optional[TreeNode]
+        :rtype: bool
+        """
+        
+        def checkNode(node):
+            if not node:
                 return False
             
-            if isIdentical(node1,subRoot):
+            if isIdentical(node,subRoot):
                 return True
             
-            return checkNode(node1.left) or checkNode(node1.right)
+            return checkNode(node.left) or checkNode(node.right)
         
         def isIdentical(node1,node2):
             if not node1 or not node2:
@@ -22,5 +28,3 @@ class Solution:
             return (node1.val==node2.val and isIdentical(node1.left,node2.left) and isIdentical(node1.right,node2.right))
         
         return checkNode(root)
-
-        
