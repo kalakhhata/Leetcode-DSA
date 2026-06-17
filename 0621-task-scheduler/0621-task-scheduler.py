d52beb4ed76f9cadec20dae652dfdba7fbc24641
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-06-17 08:13:34 -0400

    Time: 173 ms (26.23%), Space: 20.9 MB (15.92%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -6,22 +6,18 @@ class Solution:
         max_heap=[]
         for key in freq:
             heapq.heappush(max_heap,-freq[key])
-        
         q=deque()
         i=0
 
-        while q or max_heap:
+        while max_heap or q:
             if max_heap:
                 t=-heapq.heappop(max_heap)-1
                 if t!=0:
-                    q.append([t,i+n])
+                    q.append([-t,i+n])
             
             if q and q[0][1]==i:
-                t1,_=q.popleft()
-                heapq.heappush(max_heap,-t1)
+                rt,_=q.popleft()
+                heapq.heappush(max_heap,rt)
             i+=1
-        
         return i
-
-
         
