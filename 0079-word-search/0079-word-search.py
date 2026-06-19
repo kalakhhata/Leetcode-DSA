7ff95b17fa055166ff0e5146b4d82f16c065ff39
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-06-19 10:20:29 -0400

    Time: 3439 ms (72.07%), Space: 19.4 MB (56.88%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,33 +1,43 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        m=len(board)
-        n=len(board[0])
-        l=len(word)
+        n=len(board)
+        m=len(board[0])
 
-        def dfsBT(r,c,idx):
-            if idx==l:
+
+
+
+
+        def isExist(r,c,idx):
+            if idx==len(word):
                 return True
             
-            if r<0 or c<0 or r>=m or c>=n or board[r][c]!=word[idx]:
+            if r<0 or c<0 or r>=n or c>=m or word[idx]!=board[r][c]:
                 return False
             
             temp=board[r][c]
             board[r][c]='@'
-
-            found=(
-                dfsBT(r+1,c,idx+1) or
-                dfsBT(r-1,c,idx+1) or
-                dfsBT(r,c+1,idx+1) or
-                dfsBT(r,c-1,idx+1)
+            ans=  (
+                isExist(r,c+1,idx+1) or
+                isExist(r,c-1,idx+1) or
+                isExist(r+1,c,idx+1) or
+                isExist(r-1,c,idx+1)
             )
             board[r][c]=temp
-            return found
+            return ans
         
-        for i in range(m):
-            for j in range(n):
-                if board[i][j]==word[0] and dfsBT(i,j,0):
+
+        for i in range(n):
+            for j in range(m):
+                if word[0]==board[i][j] and isExist(i,j,0):
                     return True
         
         return False
+
+
+
+
+
+
+
 
         
