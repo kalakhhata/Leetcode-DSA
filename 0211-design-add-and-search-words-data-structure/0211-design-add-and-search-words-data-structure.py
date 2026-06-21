27f5bf65d769f39ef462595481bc995f2bc18593
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-06-21 09:04:49 -0400

    Time: 1063 ms (30.53%), Space: 68.4 MB (51.96%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,4 +1,4 @@
-class TrieNode:
+class TrieNode():
     def __init__(self):
         self.children={}
         self.end=False
@@ -10,36 +10,33 @@ class WordDictionary:
 
     def addWord(self, word: str) -> None:
         curr=self.root
+
         for ch in word:
             if ch not in curr.children:
                 curr.children[ch]=TrieNode()
             curr=curr.children[ch]
-        curr.end=True        
+        curr.end=True
+        
 
     def search(self, word: str) -> bool:
-        
-        
+        curr=self.root
         def dfs(j,node):
-            curr=node
-
+            
             for i in range(j,len(word)):
                 ch=word[i]
-
                 if ch=='.':
-
-                    for child in curr.children.values():
+                    for child in node.children.values():
                         if dfs(i+1,child):
                             return True
-                        
                     return False
+            
                 else:
-                    if ch not in curr.children:
+                    if ch not in node.children:
                         return False
-                    curr=curr.children[ch]
-            return curr.end
+                    node=node.children[ch]
+            return node.end
 
-        return dfs(0,self.root)
-
+        return dfs(0,curr)
         
 
 
