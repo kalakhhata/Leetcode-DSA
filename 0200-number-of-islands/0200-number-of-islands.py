93de8d1aba3d0a0164f72e4bae2bef548dc44756
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-06-24 06:20:39 -0400

    Time: 242 ms (62.83%), Space: 21.6 MB (48.25%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,27 +2,23 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return 0
+        num_of_islands=0
         
         
-        ans=0
+        def dfs(r,c):
+            if r<0 or c<0 or r>=len(grid) or c>=len(grid[0]) or grid[r][c]!="1":
+                return
+            
+            grid[r][c]="-1"
+            
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j]=='1':
-                    ans+=1
-                    self.dfs(i,j,grid)
-        
-        return ans
-
-    def dfs(self,r,c,grid):
-        if (r<0 or c<0 or r>=len(grid) or c>=len(grid[0]) or grid[r][c]!='1'):
-            return
-        grid[r][c]='-1'
-        self.dfs(r+1,c,grid)
-        self.dfs(r-1,c,grid)
-        self.dfs(r,c+1,grid)
-        self.dfs(r,c-1,grid)
-        
-
-
-        
+                if grid[i][j]=="1":
+                    num_of_islands+=1
+                    dfs(i,j)
+        return num_of_islands
         
