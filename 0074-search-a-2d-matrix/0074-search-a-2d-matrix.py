6b7db9dd27425d20c1d731dbf3ac4620c1a8625e
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-06-25 13:27:21 -0400

    Time: 0 ms (100%), Space: 19.6 MB (38.6%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,24 +1,20 @@
-class Solution(object):
-    def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
-        def getTarget(idx):
-            low=0
-            high=len(matrix[idx])-1
-            while low<=high:
-                mid=(low+high)//2
-                if matrix[idx][mid]>target:
-                    high=mid-1
-                elif matrix[idx][mid]<target:
-                    low=mid+1
-                else:
-                    return True
-            return False
-        for i in range(len(matrix)):
-            if getTarget(i):
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+        l=0
+        r=(len(matrix)*len(matrix[0]))-1
+        col=len(matrix[0])
+
+        while l<=r:
+            mid=l+(r-l)//2
+            val=matrix[mid//col][mid%col]
+            if val==target:
                 return True
+            elif val>target:
+                r=mid-1
+            else:
+                l=mid+1
+        
         return False
+
         
