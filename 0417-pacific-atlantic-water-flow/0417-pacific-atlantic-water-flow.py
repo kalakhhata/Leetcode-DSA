47d7f9be5db97d52d4782bfe5a2ffe2a7fef64b9
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-07-02 10:34:20 -0400

    Time: 23 ms (89.1%), Space: 20.8 MB (75.09%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,45 +1,40 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+
         row=len(heights)
         col=len(heights[0])
 
         pq=deque()
         pse=set()
-
         aq=deque()
         ase=set()
 
         for i in range(row):
             pq.append([i,0])
-            aq.append([i,col-1])
             pse.add((i,0))
+            aq.append([i,col-1])
             ase.add((i,col-1))
         
         for j in range(col):
             pq.append([0,j])
-            aq.append([row-1,j])
             pse.add((0,j))
+            aq.append([row-1,j])
             ase.add((row-1,j))
-
         
-        
-        def bfs(que,seen):
-            while que:
-                i,j=que.popleft()
-                for ioff,joff in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
-                    r=i+ioff
-                    c=j+joff
-                    if 0<=r<row and 0<=c<col and heights[r][c]>=heights[i][j] and (r,c) not in seen:
+        def bfs(q,seen):
+            while q:
+                i,j=q.popleft()
+                for ioff,joff in [(1,0),(-1,0),(0,1),(0,-1)]:
+                    r,c=i+ioff,j+joff
+                    if 0<=r<row and 0<=c<col and (r,c) not in seen and heights[r][c]>=heights[i][j]:
+                        q.append([r,c])
                         seen.add((r,c))
-                        que.append([r,c])
         
         bfs(pq,pse)
         bfs(aq,ase)
 
         return list(pse.intersection(ase))
 
-                    
 
-        
-        
+
         
