d75af7f468a28e2474ad339af3dfd6432cc47666
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-07-02 09:54:31 -0400

    Time: 42 ms (6.21%), Space: 19.3 MB (41.22%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,39 +1,39 @@
-from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
 
 
-
+       
+        
         q=deque()
         fresh=0
-        time=0
+        n=len(grid)
+        m=len(grid[0])
 
-        def check(r,c):
-            nonlocal fresh
-            if r<0 or c<0 or r>=len(grid) or c>=len(grid[0]) or grid[r][c]!=1:
-                return
-            q.append([r,c])
-            grid[r][c]=2
-            fresh-=1
-        
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        for i in range(n):
+            for j in range(m):
                 if grid[i][j]==2:
-                    q.append([i,j])
+                    q.append((i,j))
                 elif grid[i][j]==1:
                     fresh+=1
-        
-        while q and fresh:
-            for _ in range(len(q)):
-                r,c=q.popleft()
+        def addOranges(r,c):
+            nonlocal fresh
+            if r<0 or c<0 or r>=n or c>=m or grid[r][c]!=1:
+                return
+            grid[r][c]=2
+            q.append((r,c))
+            fresh-=1
 
-                check(r+1,c)
-                check(r-1,c)
-                check(r,c+1)
-                check(r,c-1)
-            
+        time=0
+        while q and fresh:
+
+            for i in range(len(q)):
+                r,c=q.popleft()
+                addOranges(r+1,c)
+                addOranges(r-1,c)
+                addOranges(r,c+1)
+                addOranges(r,c-1)
             time+=1
         
-        return time if fresh<=0 else -1
+        return -1 if fresh!=0 else time
 
         
