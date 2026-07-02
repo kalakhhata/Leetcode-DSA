348aca2cd164714ee4ed8d006d8dedcd36366243
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-07-02 09:26:52 -0400

    Time: 1155 ms (7.91%), Space: 110.3 MB (5.05%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -1,41 +1,28 @@
-from collections import deque
 class Solution:
     def wallsAndGates(self, rooms: List[List[int]]) -> None:
         """
         Do not return anything, modify rooms in-place instead.
         """
-
-
-
-        
-        
+        n=len(rooms)
+        m=len(rooms[0])
         q=deque()
-        visited=set()
-
-        def addRoom(r,c):
-            if r<0 or c<0 or r>=len(rooms) or c>=len(rooms[0]) or (r,c) in visited or rooms[r][c]==-1:
-                return
-            
-            q.append([r,c])
-            visited.add((r,c))
-
         for i in range(len(rooms)):
             for j in range(len(rooms[0])):
                 if rooms[i][j]==0:
-                    q.append([i,j])
-                    visited.add((i,j))
-        dist=0
-        while q:
-            for _ in range(len(q)):
-                r,c=q.popleft()
-                rooms[r][c]=dist
-                addRoom(r+1,c)
-                addRoom(r-1,c)
-                addRoom(r,c+1)
-                addRoom(r,c-1)
-            dist+=1
+                    q.append((0,i,j))
         
+        while q:
+            for i in range(len(q)):
+                d,r,c=q.popleft()
+                rooms[r][c]=min(d,rooms[r][c])
 
-                    
+                if r+1<n and rooms[r+1][c]==2147483647:
+                    q.append((d+1,r+1,c))
+                if r-1>=0 and rooms[r-1][c]==2147483647:
+                    q.append((d+1,r-1,c))
+                if c+1<m and rooms[r][c+1]==2147483647:
+                    q.append((d+1,r,c+1))
+                if c-1>=0 and rooms[r][c-1]==2147483647:
+                    q.append((d+1,r,c-1))
         
         
