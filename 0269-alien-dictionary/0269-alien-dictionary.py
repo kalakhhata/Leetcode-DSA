b9cd75a1bc39cad533b7334618e7207ba80d4bb2
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-07-04 15:03:05 -0400

    Time: 0 ms (100%), Space: 19.3 MB (86.58%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.py
+++ 0269-alien-dictionary/0269-alien-dictionary.py
@@ -1,43 +1,41 @@
-from collections import defaultdict, deque
-
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        adj = defaultdict(set)
-        indeg = {c:0 for word in words for c in word}
+        adj=defaultdict(set)
+        indeg={c:0 for word in words for c in word}
 
-        # build graph
+        #build Graph
         for i in range(len(words)-1):
-            w1 = words[i]
-            w2 = words[i+1]
+            w1=words[i]
+            w2=words[i+1]
 
-            if len(w1) > len(w2) and w1.startswith(w2):
+            #check if the first word is greater than second one and the second word is prefix of it
+            if len(w1)>len(w2) and w1.startswith(w2):
                 return ''
-
-            for c1, c2 in zip(w1, w2):
-                if c1 != c2:
+            
+            for c1,c2 in zip(w1,w2):
+                if c1!=c2:
                     if c2 not in adj[c1]:
+                        indeg[c2]+=1
                         adj[c1].add(c2)
-                        indeg[c2] += 1
                     break
-
-        # topological sort
-        q = deque()
+        #topological sort
+        q=deque()
         for c in indeg:
-            if indeg[c] == 0:
+            if indeg[c]==0:
                 q.append(c)
-
-        res = ""
-
+        ans=''
         while q:
-            c = q.popleft()
-            res += c
+            node=q.popleft()
+            ans+=node
 
-            for ne in adj[c]:
-                indeg[ne] -= 1
-                if indeg[ne] == 0:
+            for ne in adj[node]:
+                indeg[ne]-=1
+                if indeg[ne]==0:
                     q.append(ne)
-
-        if len(res) != len(indeg):
+        
+        if len(ans)!=len(indeg):
             return ''
+        
+        return ans
 
-        return res
+        
