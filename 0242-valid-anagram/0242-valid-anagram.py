8929bb257ae892e0c2993bf85da45e89e7e24b53
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-07-07 18:25:05 -0400

    Time: 11 ms (77.1%), Space: 19.4 MB (45.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,16 +2,15 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s)!=len(t):
             return False
-        bucket=[0]*26
-
+        bucket=defaultdict(int)
         for c1,c2 in zip(s,t):
-            bucket[ord(c1)-ord('a')]+=1
-            bucket[ord(c2)-ord('a')]-=1
+            bucket[c1]+=1
+            bucket[c2]-=1
         
-        for i in range(len(bucket)):
-            if bucket[i]!=0:
+        for key in bucket:
+            if bucket[key]!=0:
                 return False
+        
         return True
-
 
         
