5dd63141f096c7635e63e83e3bc044d2316724a6
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-07-07 20:40:06 -0400

    Time: 16 ms (27.75%), Space: 24.1 MB (9.29%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-
         bucket=defaultdict(list)
-    
+        
+
         for st in strs:
             freq=[0]*26
-
-            for s in st:
-                freq[ord(s)-ord('a')]+=1
+            for c in st:
+                freq[ord(c)-ord('a')]+=1
             bucket[tuple(freq)].append(st)
         
         return [bucket[key] for key in bucket]
