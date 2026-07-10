28664231b99a29669e2083e29931592671568716
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-07-10 12:11:25 -0400

    Time: 4 ms (7.82%), Space: 19.5 MB (6.49%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,15 +1,15 @@
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
-        pref=strs[0]
-        pref_len=len(pref)
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
 
-        for i in range(1,len(strs)):
-            s=strs[i]
-            while s[:pref_len]!=pref[:pref_len] and pref_len!=0:
-                pref_len-=1
+
+        ans=strs[0]
+        for i,s in enumerate(strs):
+            j=0
+            while j<len(ans) and j<len(s) and s[j]==ans[j]:
+                j+=1
+            ans=ans[:j]
+        return ans
+            
+
+
         
-        return pref[:pref_len]
