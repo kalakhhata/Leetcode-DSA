adcdde181f5ea1b1938d50d982e85207e8384fff
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-07-21 17:52:32 -0400

    Time: 125 ms (46.21%), Space: 34.3 MB (13.16%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -9,19 +9,18 @@ class NumMatrix:
             for c in range(cols):
                 top=self.prefix[r-1][c] if r>0 else 0
                 left=self.prefix[r][c-1] if c>0 else 0
-                diag=self.prefix[r-1][c-1] if r>0 and c>0 else 0
+                dig=self.prefix[r-1][c-1] if r>0 and c>0 else 0
 
-                self.prefix[r][c]=matrix[r][c]+top+left-diag
+                self.prefix[r][c]=matrix[r][c]+top+left-dig
+
         
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        total=self.prefix[row2][col2]
-
         top=self.prefix[row1-1][col2] if row1>0 else 0
         left=self.prefix[row2][col1-1] if col1>0 else 0
-        diag=self.prefix[row1-1][col1-1] if row1>0 and col1>0 else 0
+        dig=self.prefix[row1-1][col1-1] if row1>0 and col1>0 else 0
 
-        return total-top-left+diag
+        return self.prefix[row2][col2]-top-left+dig
         
 
 
