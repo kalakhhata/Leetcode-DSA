61c8692548c6d98eb7d8b921aea1e0a2d4c5623a
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-07-22 14:02:36 -0400

    Time: 3 ms (55.73%), Space: 20.5 MB (73.02%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,20 +1,19 @@
-class Solution(object):
-    def evalRPN(self, tokens):
-        st = []
-
-        ops = {
-            '+': lambda a,b: a + b,
-            '-': lambda a,b: a - b,
-            '*': lambda a,b: a * b,
-            '/': lambda a,b: int(a / b) if a * b > 0 else - (abs(a) // abs(b))
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        st=[]
+        ops={
+            '+':lambda a,b:a+b,
+            '-':lambda a,b:a-b,
+            '*':lambda a,b:a*b,
+            '/':lambda a,b:int(a/b) if a*b>0 else - (abs(a)//abs(b))
         }
 
-        for t in tokens:
-            if t in ops:
-                b = st.pop()
-                a = st.pop()
-                st.append(ops[t](a, b))
+        for token in tokens:
+            if token in ops:
+                b=st.pop()
+                a=st.pop()
+                st.append(ops[token](a,b))
             else:
-                st.append(int(t))
-
-        return st[-1]
+                st.append(int(token))
+        return st[0]
+         
