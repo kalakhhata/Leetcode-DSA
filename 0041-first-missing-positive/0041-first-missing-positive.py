462361eba719a7558c4c62535ce067642ae5d516
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-08-05 08:19:27 -0400

    Time: 19 ms (88.02%), Space: 31 MB (48.12%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,14 +1,15 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        bucket=[False]*(len(nums)+1)
-
+        bucket=[0]*(len(nums)+1)
         for num in nums:
             if 0<num<=len(nums):
-                bucket[num]=True
+                bucket[num]=1
         
         for i in range(1,len(nums)+1):
-            if not bucket[i]:
+            if bucket[i]==0:
                 return i
+        
         return len(nums)+1
 
+        
         
