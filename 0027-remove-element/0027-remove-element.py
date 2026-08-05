5064bb0a8f5c4ccb81d539bdf860e12da0c57ff0
kalakhhata <112490873+kalakhhata@users.noreply.github.com>
2026-08-05 18:53:30 -0400

    Time: 0 ms (100%), Space: 12.3 MB (89.78%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -6,14 +6,8 @@ class Solution(object):
         :rtype: int
         """
         l=0
-        r=len(nums)-1
-        
-
-        while l<=r:
-            if nums[l]!=val:
+        for r in range(len(nums)):
+            if nums[r]!=val:
+                nums[l]=nums[r]
                 l+=1
-            else:
-                nums[l],nums[r]=nums[r],nums[l]
-                r-=1
         return l
-        
